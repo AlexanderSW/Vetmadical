ee44afbc004d3ee3a92edf0036dfd8060718b128
Alexander Podobulkin <Alexander Podobulkin>
2017-05-17 09:37:10 +0300

    incorrect_login

---
+++ incorrect_login.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.support.select import Select
+import time
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import Select
+from selenium.common.exceptions import NoSuchElementException
+from selenium.common.exceptions import NoAlertPresentException
+import unittest, time, re
+
+chrome_path = r"C:\Chromedriver\chromedriver.exe"
+driver = webdriver.Chrome(chrome_path)
+driver.get('http://dev.vetmedical.ru/')
+
+driver.find_element_by_name('login').clickAndWait
+driver.find_element_by_name('login').send_keys('AlexanderSW')
+
+
+
+
+
+
+
